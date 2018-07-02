8beeeaaaf684c2665eb46b032f55c54174b1589c
LegendaryZealot <875626439@qq.com>
2018-07-02 01:58:42 +0000

    fix wornning

--- tcp/define.h
+++ tcp/define.h
@@ -3,3 +3,7 @@
 #define MAXDATASIZE 128
 #define SERVER_GREET "hi,from server!"
 #define SERVER_IP "127.0.0.1"
+
+#include <arpa/inet.h>
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
